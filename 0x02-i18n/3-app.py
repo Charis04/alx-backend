5b617a77f7273f8ad7d23bab7d5fa44cbd61fb38
charis <charisadu@gmail.com>
2024-11-09 03:55:59 +0100

    paremeterise templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+A basic flask app with basic babel set up
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for intiating babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Function that determines the best language for user"""
+    return request.accept_languages.best_match(app.config['Languages'])
+
+
+@app.route('/')
+def home():
+    """Route to homepage"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
